1ab6c0cf81f995808a5e31ecf93a5277dd4975c3
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2025-04-15 10:42:33 -0300

    main

--- src/main/java/com/example/dev/Funcionarios/Service/FuncionariosService.java
+++ src/main/java/com/example/dev/Funcionarios/Service/FuncionariosService.java
@@ -40,9 +40,6 @@ public class FuncionariosService {
     funcionariosRepository.findByCpf(request.getCpf()).ifPresent(funcionario -> {
       throw new ValidException("Funcionário já cadastrado com esse CPF!");
     });
-    funcionariosRepository.findByNome(request.getNome()).ifPresent(funcionario -> {
-      throw new ValidException("Funcionário já cadastrado com esse nome!");
-    });
     FuncionariosModel funcionario = funcionariosMapper.mapToModel(request);
     funcionario = funcionariosRepository.save(funcionario);
     return funcionariosMapper.mapToResponse(funcionario);
